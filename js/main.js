877e1d5f818ac5463597d9024fc5e9d1b8c2cde4
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2024-12-01 00:45:55 -0500

    responsive design minimal done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 window.onload = () => {
-    const cursorInnerEl = document.querySelector(".circle-in");
-    const cursorOuterEl = document.querySelector(".circle-out");
-    window.onmousemove = function (event) {
-      cursorOuterEl.style.transform = "translate(" + event.clientX + "px, " + event.clientY + "px" + ")";
-      cursorInnerEl.style.transform = "translate(" + event.clientX + "px, " + event.clientY + "px" + ")";
-    };
-  };
-  
+    (() => {
+        // Custom cursor update code:
+        const cursorInnerEl = document.querySelector(".circle-in");
+        const cursorOuterEl = document.querySelector(".circle-out");
+        window.onmousemove = function (event) {
+            cursorOuterEl.style.transform =
+                "translate(" + event.clientX + "px, " + event.clientY + "px" + ")";
+            cursorInnerEl.style.transform =
+                "translate(" + event.clientX + "px, " + event.clientY + "px" + ")";
+        };
+    })();
+};
