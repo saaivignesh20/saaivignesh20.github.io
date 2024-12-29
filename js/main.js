e3f831d5513831316471dcf569bfb957b83e91e5
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2024-12-28 20:24:03 -0500

    mobile nav done

--- js/main.js
+++ js/main.js
@@ -10,4 +10,32 @@ window.onload = () => {
                 "translate(" + event.clientX + "px, " + event.clientY + "px" + ")";
         };
     })();
+
+    let mobileNavigationOpen = false;
+    (() => {
+        const mobileNavIcon = document.querySelector("#mobile-nav-icon");
+        const mobileNavContainer = document.querySelector(".mobile-nav-container");
+        if (!mobileNavIcon || !mobileNavContainer) return;
+
+        // Assign event for the mobile nav icon
+        mobileNavIcon.onclick = function () {
+            mobileNavigationOpen = !mobileNavigationOpen;
+
+            if (mobileNavigationOpen) {
+                mobileNavIcon.src = "img/mobile-nav-icon-close.svg";
+                mobileNavContainer.classList.add("open");
+            } else {
+                mobileNavIcon.src = "img/mobile-nav-icon.svg";
+                mobileNavContainer.classList.remove("open");
+            }
+        };
+
+        // Assign event for mobile navigation links:
+        const mobileViewLinks = document.querySelectorAll(".mobile-nav > ul > li");
+        for (const link of mobileViewLinks) {
+            link.onclick = function () {
+                mobileNavIcon.click();
+            };
+        }
+    })();
 };
